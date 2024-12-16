27a9397c7cd8b106298cca53fe12039b198d65ac
goldleo1 <goldleo1@naver.com>
2024-12-16 11:35:29 +0900

    ''

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'goldleo1',
-  bio: 'newb1e',
+  bio: 'newbie on hack',
   links: [
     {
       name: 'Twitter',
