9e96e655e7f7a4fcb09bab6a1844508c2284d93b
goldleo1 <goldleo1@naver.com>
2024-12-16 10:19:35 +0900

    ''

--- astro.config.mjs
+++ astro.config.mjs
@@ -54,7 +54,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://github.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
