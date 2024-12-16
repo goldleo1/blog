5234e734b8a22075b1afadb412a96fe769432f55
goldleo1 <goldleo1@naver.com>
2024-12-16 10:58:17 +0900

    ''

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,15 +21,13 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://goldleo1.github.io/blog/",
   base: "/",
   trailingSlash: "always",
   integrations: [
-    tailwind(
-        {
-          nesting: true,
-        }
-    ),
+    tailwind({
+      nesting: true,
+    }),
     swup({
       theme: false,
       animationClass: "transition-swup-", // see https://swup.js.org/options/#animationselector
